9057617ae29bf89a0865845d5fd25982832286db
Alexey Fishler <amberdance@yandex.ru>
2023-05-05 11:43:37 +0300

    Добавил сущность InformationSystem

---
+++ src/main/java/ru/hard2code/GisDatabaseApi/model/InformationSystem.java
@@ -0,0 +1,33 @@
+package ru.hard2code.GisDatabaseApi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "gis")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class InformationSystem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "short_name", nullable = false, unique = true)
+    private String shortName;
+
+    @Column(name = "full_name", nullable = false, unique = true)
+    private String fullName;
+
+    @Column(name = "address", nullable = false, unique = true)
+    private String address;
+
+    @Column(name = "requisites", unique = true)
+    private String requisites;
+
+}
