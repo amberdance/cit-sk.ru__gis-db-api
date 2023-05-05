004232ebe95c09cedc7ee40fcd0ef595b3e73f34
Alexey Fishler <amberdance@yandex.ru>
2023-05-05 14:04:27 +0300

    Organizations done

--- src/main/java/ru/hard2code/GisDatabaseApi/controller/OrganizationController.java
+++ src/main/java/ru/hard2code/GisDatabaseApi/controller/OrganizationController.java
@@ -1,9 +1,6 @@
 package ru.hard2code.GisDatabaseApi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.hard2code.GisDatabaseApi.model.Organization;
 import ru.hard2code.GisDatabaseApi.service.organization.OrganizationService;
 
@@ -15,17 +12,32 @@ public class OrganizationController {
 
     private final OrganizationService organizationService;
 
-    public OrganizationController(OrganizationService organizationService) {
+    OrganizationController(OrganizationService organizationService) {
         this.organizationService = organizationService;
     }
 
     @GetMapping
-    List<Organization> getAllOrganizations() {
+    public List<Organization> getAllOrganizations() {
         return organizationService.findAll();
     }
 
     @GetMapping("{id}")
-    Organization getOrganizationById(@PathVariable("id") long id) {
+    public Organization getOrganizationById(@PathVariable("id") long id) {
         return organizationService.findById(id);
+    }
+
+    @PostMapping
+    public Organization createOrganization(@RequestBody Organization organization) {
+        return organizationService.create(organization);
+    }
+
+    @PutMapping("{id}")
+    public Organization updateOrganization(@PathVariable("id") long id, @RequestBody Organization organization) {
+        return organizationService.update(id, organization);
+    }
+
+    @DeleteMapping("{id}")
+    public void updateOrganization(@PathVariable("id") long id) {
+        organizationService.delete(id);
     }
 }
