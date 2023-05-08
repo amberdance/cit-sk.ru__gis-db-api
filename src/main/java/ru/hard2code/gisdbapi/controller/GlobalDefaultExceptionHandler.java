ef4ece98a094ba72f5c3ef2bcc220701e99f4507
Alexey Fishler <amberdance@yandex.ru>
2023-05-08 20:12:35 +0300

    Добавил обработку ошибок валидации

--- src/main/java/ru/hard2code/gisdbapi/controller/GlobalDefaultExceptionHandler.java
+++ src/main/java/ru/hard2code/gisdbapi/controller/GlobalDefaultExceptionHandler.java
@@ -1,42 +1,27 @@
 package ru.hard2code.gisdbapi.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.hard2code.gisdbapi.exception.EntityNotFoundException;
+import ru.hard2code.gisdbapi.util.RestErrorResponse;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleNotFoundException(EntityNotFoundException e) {
-        return new ResponseEntity<>(new ErrorInfo(e, HttpStatus.NOT_FOUND).getError(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Map<String, Object>> handleNotFoundError(EntityNotFoundException e, HttpServletRequest request) {
+        return new RestErrorResponse(request, HttpStatus.NOT_FOUND).defaultError(e);
     }
 
-    @ExceptionHandler({ConstraintViolationException.class, HttpMessageNotReadableException.class})
-    public ResponseEntity<Map<String, Object>> handleBadRequest(Exception e) {
-        return new ResponseEntity<>(new ErrorInfo(e, HttpStatus.BAD_REQUEST).getError(), HttpStatus.BAD_REQUEST);
-    }
-
-
-    private static class ErrorInfo {
-        private final Map<String, Object> error = new LinkedHashMap<>();
-
-        public ErrorInfo(Throwable e, HttpStatus httpStatus) {
-            error.put("code", httpStatus.value());
-            error.put("status", httpStatus.getReasonPhrase());
-            error.put("message", e.getMessage());
-        }
-
-        public Map<String, Object> getError() {
-            return error;
-        }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationError(ConstraintViolationException e, HttpServletRequest request) {
+        return new RestErrorResponse(request, HttpStatus.BAD_REQUEST).validationError(e);
     }
 }
