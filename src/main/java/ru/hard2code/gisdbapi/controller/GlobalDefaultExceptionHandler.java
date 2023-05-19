6605fa7eff208c650ea811d8c7822b8ba63a1977
Alexey Fishler <amberdance@yandex.ru>
2023-05-19 11:36:55 +0300

    Delete unused error handler

--- src/main/java/ru/hard2code/gisdbapi/controller/GlobalDefaultExceptionHandler.java
+++ src/main/java/ru/hard2code/gisdbapi/controller/GlobalDefaultExceptionHandler.java
@@ -5,14 +5,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.hard2code.gisdbapi.errorhandlers.RestErrorResponseHandler;
 import ru.hard2code.gisdbapi.exception.EntityNotFoundException;
-import ru.hard2code.gisdbapi.exception.ForbiddenException;
 
 import java.util.Map;
 
@@ -29,16 +26,9 @@ public class GlobalDefaultExceptionHandler extends ResponseEntityExceptionHandle
         return new RestErrorResponseHandler(request, HttpStatus.BAD_REQUEST).validationError(e);
     }
 
-    @ExceptionHandler({AccessDeniedException.class, ForbiddenException.class})
-    @ResponseBody
-    public ResponseEntity<Map<String, Object>> handleForbiddenError(AccessDeniedException e,
-                                                                    HttpServletRequest request) {
-        return new RestErrorResponseHandler(request, HttpStatus.FORBIDDEN).defaultError(e);
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleDefaultError(Exception e, HttpServletRequest request) {
-        logger.error(e.getLocalizedMessage());
+        logger.error(e.getMessage());
         return new RestErrorResponseHandler(request,
                 HttpStatus.INTERNAL_SERVER_ERROR).defaultError(e);
     }
